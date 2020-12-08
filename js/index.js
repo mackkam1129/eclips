ddcaa4544c9adc1e3e32389a3adbc0490b764eb3
mackkam1129 <62326848+mackkam1129@users.noreply.github.com>
2020-12-08 22:14:05 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,28 @@
+$(document).ready(function ()	{
+
+		console.log($(".st0"))
+		var pathArray = $(".st0")
+
+		$.each(pathArray , function(i,d){
+
+			
+			var pathLength = d.getTotalLength ()
+			console.log(pathLength)
+
+			$(d) . css("storke-dasharry" , pathLength + "" +pathLength)
+			$(d) . css("stroke - dashoffset" , pathLength)
+})
+	})
+
+
+$(document).ready(function ()	{
+$('.blue').hover(function(){
+  $('body').addClass('blue');
+ $( '#demo' ).fadeOut();
+ $( '#demo1' ).fadeOut();
+ $( 'img' ).fadeIn();
+  $( '#dash' ).fadeOut();
+  $( '#sun' ).fadeOut();
+});
+
+})
